c1efb85470a60abd9e4b965cd83267c0f84be175
leerachel5 <leerachel481@gmail.com>
2025-01-01 00:24:42 -0800

    initial commit

---
+++ Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RThemeEngineExample
+//
+//  Created by Rachel Lee on 1/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/RThemeEngineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RThemeEngineExampleApp.swift
+//  RThemeEngineExample
+//
+//  Created by Rachel Lee on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct RThemeEngineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
