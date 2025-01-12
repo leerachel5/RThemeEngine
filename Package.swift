00ce2e3faadd2ce8f8322a11f25b6633ef5f63b7
leerachel5 <leerachel481@gmail.com>
2025-01-11 18:19:28 -0800

    include resources to SPM target

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
         .target(
             name: "RThemeEngine",
-            path: "Sources"
+            path: "Sources",
+            resources: [.process("Resources")]
         )
     ]
 )
