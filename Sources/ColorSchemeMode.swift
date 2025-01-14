a7383f75459b0e6fb081e91bd2a5ea148c23deed
leerachel5 <leerachel481@gmail.com>
2025-01-14 00:09:39 -0800

    Revert "create helper func to apply ColorSchemeMode to window"
    This reverts commit 894c0f6397ff21fbd9c4d2f8bec037cfadb37708.

--- Sources/ColorSchemeMode.swift
+++ Sources/ColorSchemeMode.swift
@@ -21,15 +21,4 @@ public enum ColorSchemeMode: String, CaseIterable, Identifiable {
         case .system: .unspecified
         }
     }
-    
-    /// Applies the specified color scheme mode (light, dark, or system) to the first window
-    /// in the current window scene. This method overrides the app's user interface style
-    /// for the window, allowing it to reflect the desired color scheme.
-    public func apply() {
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            if let window = windowScene.windows.first {
-                window.overrideUserInterfaceStyle = uiUserInterfaceStyle
-            }
-        }
-    }
 }
